b1e5ae805b75efc4aaa95324cb9075d3ffd6bb0d
Yurii Hohan <yurii@mixbook.com>
2018-05-23 17:15:06 +0300

    More unit-tests

--- book/library/book.rb
+++ book/library/book.rb
@@ -5,12 +5,12 @@ module Library
   class Book
     attr_reader :name, :author, :pages, :at_page
     PAGE_LENGTH = 800
-    NEW_LINE = '\n'.freeze
+    NEW_LINE = "\n".freeze
 
     def initialize(lines, name, author)
-      raise ArgumentError, 'Book contains at least one line' if lines.empty?
-      raise ArgumentError, 'Book should have non-empty name' if name.empty?
-      raise ArgumentError, 'Book should have an author' if author.empty?
+      raise ArgumentError, 'Book contains at least one line' if check_non_empty(lines)
+      raise ArgumentError, 'Book should have non-empty name' if check_non_empty(name)
+      raise ArgumentError, 'Book should have an author' if check_non_empty(author)
 
       distribute_to_pages(lines)
       @name = name
@@ -19,7 +19,7 @@ module Library
     end
 
     def page(page_number)
-      print_page(page_number)
+      navigate_page(page_number)
     end
 
     def total_pages
@@ -27,7 +27,7 @@ module Library
     end
 
     def current_page
-      print_page(@at_page)
+      navigate_page(@at_page)
     end
 
     def bookmark(page_number)
@@ -40,34 +40,34 @@ module Library
 
     def next_page
       @at_page += 1
-      print_page(@at_page)
+      navigate_page(@at_page)
     end
 
     def prev_page
       @at_page -= 1
-      print_page(@at_page)
+      navigate_page(@at_page)
     end
 
     def first_page
       @at_page = 1
-      print_page(@at_page)
+      navigate_page(@at_page)
     end
 
     def last_page
       @at_page = total_pages - 1
-      print_page(@at_page)
+      navigate_page(@at_page)
     end
 
     def skip_pages(page_number)
       @at_page += page_number
-      print_page(@at_page)
+      navigate_page(@at_page)
     end
 
     private def distribute_to_pages(lines)
       @pages = {}
       page_count = 1
       page_text = ''
-
+      
       lines.each do |line|
         line.split.each do |word|
           if page_text.size + word.size > PAGE_LENGTH
@@ -84,15 +84,22 @@ module Library
       @pages[page_count] = Page.new(page_text) unless page_text.empty?
     end
 
-    private def print_page(page_number)
-      puts @pages[page_number].text
-      puts page_number.to_s.rjust(winsize[1])
+    private def navigate_page(page_number)
+      raise ArgumentError, 'Cannot navigate to negative page' if page_number <= 0 || page_number > total_pages
+
+      page_preview = @pages[page_number].text
+      page_preview << NEW_LINE
+      page_preview << page_number.to_s.rjust(winsize[1])
     end
 
     private def winsize
       # Ruby 1.9.3 added 'io/console' to the standard library.
       require 'io/console'
       IO.console.winsize
+    end
+
+    private def check_non_empty(arg)
+      arg.nil? || arg.empty?
     end
   end
 end
--- book/spec/book_spec.rb
+++ book/spec/book_spec.rb
@@ -7,26 +7,63 @@ RSpec.describe Library::Book do
 
     BOOK_TITLE = 'Book Title'.freeze
     BOOK_AUTHOR = 'Book Author'.freeze
-    LINES = [Randomizer.random_string(300), Randomizer.random_string(300), 
-            Randomizer.random_string(300)]
-    let!(:book) do
-      Library::Book.new(LINES, BOOK_TITLE, BOOK_AUTHOR)
+    LINES = [Randomizer.random_string(300),
+             Randomizer.random_string(300),
+             Randomizer.random_string(300)].freeze
+
+    describe 'when book cannot be created' do
+      it 'should throw on attempt to create empty book' do
+        expect { Library::Book.new(nil, BOOK_TITLE, BOOK_AUTHOR) }.to raise_error(ArgumentError)
+        expect { Library::Book.new([], BOOK_TITLE, BOOK_AUTHOR) }.to raise_error(ArgumentError)
+      end
+
+      it 'should throw on attempt to create book without title' do
+        expect { Library::Book.new(LINES, nil, BOOK_AUTHOR) }.to raise_error(ArgumentError)
+        expect { Library::Book.new(LINES, '', BOOK_AUTHOR) }.to raise_error(ArgumentError)
+      end
+
+      it 'should throw on attempt to create book without book author' do
+        expect { Library::Book.new(LINES, BOOK_TITLE, nil) }.to raise_error(ArgumentError)
+        expect { Library::Book.new(LINES, BOOK_TITLE, '') }.to raise_error(ArgumentError)
+      end
     end
 
-    it 'should have two pages' do
-      expect(book.total_pages).to eq 2
-    end
+    describe 'when book can be created' do
+      let!(:book) do
+        Library::Book.new(LINES, BOOK_TITLE, BOOK_AUTHOR)
+      end
 
-    it 'should set book title' do
-      expect(book.name).to eq BOOK_TITLE
-    end
+      it 'should have two pages' do
+        expect(book.total_pages).to eq 2
+      end
 
-    it 'should set book author' do
-      expect(book.author).to eq BOOK_AUTHOR
-    end
-    
-    it 'should set at page to 1' do
-      expect(book.at_page).to eq 1
+      it 'should set book title' do
+        expect(book.name).to eq BOOK_TITLE
+      end
+
+      it 'should set book author' do
+        expect(book.author).to eq BOOK_AUTHOR
+      end
+
+      it 'should set at page to 1' do
+        expect(book.at_page).to eq 1
+      end
+
+      it 'should contain two new lines and a line for page number on first page' do
+        expect(book.page(1).count("\n")).to eq 2 + 1
+      end
+
+      it 'should contain one line and a line for page number on first page' do
+        expect(book.page(2).count("\n")).to eq 1 + 1
+      end
+
+      it 'should throw on leaving the bounds of the book downwards' do
+        expect { book.prev_page }.to raise_error(ArgumentError)
+      end
+
+      it 'should throw on leaving the bounds of the book upwards' do
+        expect { book.page(3) }.to raise_error(ArgumentError)
+      end
     end
   end
 end
