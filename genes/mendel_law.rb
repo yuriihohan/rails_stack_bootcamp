312a26230fd917bfe623396c44670566f4114728
Yurii Hohan <yurii@mixbook.com>
2018-05-28 14:26:18 +0300

    Modify probability to use Rational

--- genes/mendel_law.rb
+++ genes/mendel_law.rb
@@ -1,25 +1,26 @@
 require './gene_probability'
 # Class defining First Mendel Law
 class MendelLaw
-  @gene_combination_rules = {}
-  @gene_combination_rules[GeneCombination.new(:dominant, :dominant)] = [GeneProbability.new(:dominant, 100)]
-  @gene_combination_rules[GeneCombination.new(:dominant, :heterozigote)] = [GeneProbability.new(:dominant, 50),
-                                                                            GeneProbability.new(:heterozigote, 50)]
-  @gene_combination_rules[GeneCombination.new(:heterozigote, :dominant)] = [GeneProbability.new(:dominant, 50),
-                                                                            GeneProbability.new(:heterozigote, 50)]
-  @gene_combination_rules[GeneCombination.new(:heterozigote, :heterozigote)] = [GeneProbability.new(:heterozigote, 50),
-                                                                                GeneProbability.new(:dominant, 25),
-                                                                                GeneProbability.new(:recessive, 25)]
-  @gene_combination_rules[GeneCombination.new(:dominant, :recessive)] = [GeneProbability.new(:heterozigote, 100)]
-  @gene_combination_rules[GeneCombination.new(:recessive, :dominant)] = [GeneProbability.new(:heterozigote, 100)]
-  @gene_combination_rules[GeneCombination.new(:heterozigote, :recessive)] = [GeneProbability.new(:recessive, 50),
-                                                                             GeneProbability.new(:heterozigote, 50)]
-  @gene_combination_rules[GeneCombination.new(:recessive, :heterozigote)] = [GeneProbability.new(:recessive, 50),
-                                                                             GeneProbability.new(:heterozigote, 50)]
-  @gene_combination_rules[GeneCombination.new(:recessive, :recessive)] = [GeneProbability.new(:recessive, 100)]
+  @mendel_rules = {}
+  @mendel_rules[GeneCombination.new(:dominant, :dominant)] = [GeneProbability.new(:dominant, Rational(1))]
+  @mendel_rules[GeneCombination.new(:dominant, :heterozigote)] = [GeneProbability.new(:dominant, Rational(1, 2)),
+                                                                  GeneProbability.new(:heterozigote, Rational(1, 2))]
+  @mendel_rules[GeneCombination.new(:heterozigote, :dominant)] = [GeneProbability.new(:dominant, Rational(1, 2)),
+                                                                  GeneProbability.new(:heterozigote, Rational(1, 2))]
+  @mendel_rules[GeneCombination.new(:heterozigote, :heterozigote)] = [GeneProbability.new(:heterozigote,
+                                                                                          Rational(1, 2)),
+                                                                      GeneProbability.new(:dominant, Rational(1, 4)),
+                                                                      GeneProbability.new(:recessive, Rational(1, 4))]
+  @mendel_rules[GeneCombination.new(:dominant, :recessive)] = [GeneProbability.new(:heterozigote, Rational(1))]
+  @mendel_rules[GeneCombination.new(:recessive, :dominant)] = [GeneProbability.new(:heterozigote, Rational(1))]
+  @mendel_rules[GeneCombination.new(:heterozigote, :recessive)] = [GeneProbability.new(:recessive, Rational(1, 2)),
+                                                                   GeneProbability.new(:heterozigote, Rational(1, 2))]
+  @mendel_rules[GeneCombination.new(:recessive, :heterozigote)] = [GeneProbability.new(:recessive, Rational(1, 2)),
+                                                                   GeneProbability.new(:heterozigote, Rational(1, 2))]
+  @mendel_rules[GeneCombination.new(:recessive, :recessive)] = [GeneProbability.new(:recessive, Rational(1))]
 
   def self.combine(father_gene, mother_gene)
-    @gene_combination_rules[GeneCombination.new(father_gene.allele_type,
-                                                mother_gene.allele_type)]
+    @mendel_rules[GeneCombination.new(father_gene.allele_type,
+                                      mother_gene.allele_type)]
   end
 end
--- genes/run.rb
+++ genes/run.rb
@@ -9,7 +9,8 @@ a_feature, t_feature = father.combine(mother)
 
 a_feature.each do |a_gene|
   t_feature.each do |t_gene|
+    child_probability = a_gene.probability * t_gene.probability
     puts "#{horse_dictionary.get_breed_name(HorseBreed.new(a_gene.allele_type, t_gene.allele_type))[0]} " \
-          "#{((a_gene.probability * t_gene.probability) / 10_000.0)}"
+      "#{child_probability} (#{child_probability.to_f})"
   end
 end
