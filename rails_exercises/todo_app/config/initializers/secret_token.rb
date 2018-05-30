c0820637cca6a6634937540056f564f5c730af67
Yurii Hohan <yurii@mixbook.com>
2018-05-30 11:20:00 +0300

    Todo App Skeleton commit - Rails 3.2.22

---
+++ rails_exercises/todo_app/config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# rubocop:disable LineLength
+TodoApp::Application.config.secret_token = '87b8ee81f7ada377a598aadd68a8ff70834441edcfc715ace10083b2ba5c7f6346c995d8755c57eee70ddaba9d78bacf4f8a27813ef5a99f79e355f899ce75b8'
+# rubocop:enable LineLength
