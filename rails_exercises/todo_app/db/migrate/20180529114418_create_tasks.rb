09ce796a245e7fd54b75060e7e883b0882e8375c
Yurii Hohan <yurii@mixbook.com>
2018-05-30 10:59:36 +0300

    Remove rails 5 todo app, will switch to 3.2

--- rails_exercises/todo_app/db/migrate/20180529114418_create_tasks.rb
+++
@@ -1,9 +0,0 @@
-class CreateTasks < ActiveRecord::Migration[5.1]
-  def change
-    create_table :tasks do |t|
-      t.string :title
-      t.text :note
-      t.date :completed
-    end
-  end
-end
