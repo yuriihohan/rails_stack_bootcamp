98a2dc75a637f60a104f6331846de7338f2848d4
Yurii Hohan <yurii@mixbook.com>
2018-06-04 17:18:39 +0300

    Ecommerce project skeleton

---
+++ ecommerce_front/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommerceFront::Application.config.secret_token = 'ca19590abd46b14f7a0f80b943f0ef50ddf81d322e3f21d324c95800cc453a7bfe3f14c93abb9eaca8934ebfa5a2e6fe1ac2380a6751f8a0459d06ee6f25b9d6'
