a381a453740867d156f8494b97e3b4d67b3a6f8d
Yurii Hohan <yurii@mixbook.com>
2018-06-04 16:21:57 +0300

    Controller logic tests

---
+++ rails_exercises/todo_app/spec/controllers/task_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe TaskController do
+  
+
+  describe 'post to create task' do
+    let(:validTaskParams) { { 'task' => {'title' => 'new valid task', 'dueDate' => Date.today } }}
+    let(:invalidTaskParams) { { 'task' => {'title' => nil, 'dueDate' => Date.today } }}
+    let(:taskController) { TaskController.new }
+  
+   context 'when task is valid' do
+     it 'should render list page' do
+       post :create, validTaskParams
+    
+       expect(response).to redirect_to('/')
+     end
+    end
+    context 'when task is invalid' do
+      it 'should redirect to list action' do
+        post :create, invalidTaskParams
+        
+        expect(response).to render_template('new')
+        expect(response).to be_ok
+      end
+    end
+  end
+
+  describe 'post to update task' do
+
+    let(:task) {
+      task = Task.new( { 'title' => 'old title', 'dueDate' => Date.today } )
+      task.save!
+      task
+    }
+
+    let(:validTaskParams) { { 'task' => { 'id' => task.id, 'title' => 'new valid task', 'dueDate' => Date.today } }}
+    let(:invalidTaskParams) { { 'task' => { 'id' => task.id, 'title' => nil, 'dueDate' => Date.today } }}
+    context 'when task is valid' do
+      it 'should render list page' do
+        post :update, validTaskParams
+     
+        expect(response).to redirect_to('/')
+      end
+    end
+     context 'when task is invalid' do
+       it 'should redirect to list action' do
+         post :update, invalidTaskParams
+         
+         expect(response).to render_template('new')
+         expect(response).to be_ok
+      end
+    end
+  end
+end
