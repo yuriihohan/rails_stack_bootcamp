5260c403709d413fe078813cd0e72e3f41731f5e
Yurii Hohan <yurii@mixbook.com>
2018-06-11 11:19:53 +0300

    Fix form id for file edits

--- rails_exercises/ecommerce_front/admin/app/assets/javascripts/admin/slider_images.js
+++ rails_exercises/ecommerce_front/admin/app/assets/javascripts/admin/slider_images.js
@@ -1,7 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).ready(function () {
-    is_newForm = $('form').attr('id') === 'new_inspiration_block';
+    is_newForm = $('form').attr('id') === 'new_slider_iamge';
     if (!is_newForm) {
         $('.re_upload').change(function (e) {
             var fileName = e.target.files[0].name;
